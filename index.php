aeb61c8098008a5498222aed2870a4e83ee086b1
yuttana211 <55018763+yuttana211@users.noreply.github.com>
2019-09-12 14:41:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
 	$messages = [];
 	$messages['replyToken'] = $replyToken;
-	$messages['messages'][0] = getFormatTextMessage("ต้องการไรฟะ");
+	$messages['messages'][0] = getFormatTextMessage("ใครฟะ ไม่รู้จัก");
 
 	$encodeJson = json_encode($messages);
 
